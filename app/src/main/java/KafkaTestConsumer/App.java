d26b5ef596cd7473c8f7b2a4fc6ab2eeefdcf3bf
Scott Wang <scott@wt83.com>
2021-01-31 15:21:44 -0800

    Use aws schema registry

--- app/src/main/java/KafkaTestConsumer/App.java
+++ app/src/main/java/KafkaTestConsumer/App.java
@@ -4,9 +4,9 @@
 package KafkaTestConsumer;
 
 import KafkaTest.Person;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
-import io.confluent.kafka.serializers.KafkaAvroDeserializer;
-import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig;
+import com.amazonaws.services.schemaregistry.deserializers.avro.AWSKafkaAvroDeserializer;
+import com.amazonaws.services.schemaregistry.utils.AWSSchemaRegistryConstants;
+import com.amazonaws.services.schemaregistry.utils.AvroRecordType;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
@@ -19,7 +19,7 @@ import java.util.Properties;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello Consumer!";
     }
 
     public static void main(String[] args) {
@@ -31,13 +31,15 @@ public class App {
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, "http://localhost:8081");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class);
-        props.put(KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG, true);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, AWSKafkaAvroDeserializer.class.getName());
+        props.put(AWSSchemaRegistryConstants.AWS_REGION, "us-east-1"); // Pass an AWS Region
+        props.put(AWSSchemaRegistryConstants.AVRO_RECORD_TYPE, AvroRecordType.SPECIFIC_RECORD.getName());
+        props.put(AWSSchemaRegistryConstants.CACHE_TIME_TO_LIVE_MILLIS, "86400000"); // If not passed, uses 86400000
+        props.put(AWSSchemaRegistryConstants.CACHE_SIZE, "10"); // default value is 200
 
         KafkaConsumer<String, Person> consumer = new KafkaConsumer<>(props);
-        consumer.subscribe(Collections.singletonList("person-test"));
+        consumer.subscribe(Collections.singletonList("person-test-glue"));
 
         while (true) {
             final ConsumerRecords<String, Person> records = consumer.poll(Duration.ofMillis(100));
